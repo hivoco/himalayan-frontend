b63bf9c5858386cd88ad61ee333f2c8ce5e10ee3
Krishna-hivoco <krishna@hivoco.com>
2024-08-22 21:24:09 +0530

    Animation done

--- src/components/ThankYouMsg.jsx
+++ src/components/ThankYouMsg.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import Button from "./Button";
 import axios from "../instance.js";
 
@@ -25,6 +25,13 @@ const ThankYouMsg = () => {
     getRandomMessage();
   }, []);
 
+  const [animate, setAnimate] = useState(false);
+  useLayoutEffect(() => {
+    setTimeout(() => {
+      setAnimate(true);
+    }, 500);
+  }, []);
+
   return (
     <div className="h-svh bg-flowery-bg  bg-cover  bg-center bg-no-repeat bg-fixed py-[2.125rem]  flex flex-col justify-between  gap-y-5">
       <div className="flex flex-1 flex-col items-center gap-[9px] ">
@@ -34,7 +41,13 @@ const ThankYouMsg = () => {
           alt="logo"
         />
 
-        <div className="relative  flex items-center w-[11.5625rem] h-[19.375rem] rounded-[9.6875rem] mb-2">
+        <div
+          className={`relative  flex items-center w-[11.5625rem] h-[19.375rem] rounded-[9.6875rem] mb-2 ${
+            animate
+              ? " opacity-100 scale-100 transition-all duration-500 delay-100 ease-in-out"
+              : " opacity-0 scale-0"
+          }`}
+        >
           <img
             className="absolute z-0 top-0 left-0  h-full w-full"
             src="/images/person-bg.png"
@@ -57,7 +70,13 @@ const ThankYouMsg = () => {
       </div>
 
       <div className="flex flex-1 flex-col gap-4">
-        <div className="flex flex-1 flex-col items-center gap-1">
+        <div
+          className={`flex flex-1 flex-col items-center gap-1 ${
+            animate
+              ? " opacity-100  transition-all duration-500 delay-100 ease-in-out"
+              : " opacity-0 "
+          }`}
+        >
           <div className=" flex flex-col gap-1">
             <h1 className="text-textPink font-Inter text-base font-bold leading-[1.21rem] text-center">
               Thank you for supporting us.{" "}
@@ -85,7 +104,11 @@ const ThankYouMsg = () => {
 
         <Button
           onClick={() => window.open("https://himalayansaffron.in", "_blank")}
-          className={`self-center max-w-[19.5rem] text-nowrap font-Poppins py-[14px] rounded-[14px] border-white/50 font-semibold text-[17px] leading-[22px] px-[105px]`}
+          className={`self-center max-w-[19.5rem] text-nowrap font-Poppins py-[14px] rounded-[14px] border-white/50 font-semibold text-[17px] leading-[22px] px-[105px] ${
+            animate
+              ? "translate-y-0 opacity-100 scale-100 transition-all duration-1000 delay-100 ease-in-out"
+              : "translate-y-40 opacity-0 scale-50"
+          } `}
           // navigateUrl={"/batch-input"}
           title={"Explore Now"}
         />
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,33 +1,58 @@
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
+import { useEffect, useState } from "react";
 
 const Welcome = () => {
   const navigate = useNavigate();
-  // bg-[length:100%_100%] cover cent percent width and height of the container
-  //  bg-flowery-bg  bg-cover  bg-center bg-no-repeat bg-fixed
+  const [animate, setAnimate] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimate(true);
+    }, 500);
+  }, []);
   return (
     <div className="h-svh bg-flowery-bg  bg-cover  bg-center bg-no-repeat bg-fixed  flex flex-col gap-y-[107x] justify-between pt-[2.1375rem] pb-[0.89375rem]">
       {/* <div className="flex flex-col gap-y-[124px]"> */}
       <Button
         onClick={() => window.open("https://himalayansaffron.in", "_blank")}
-        className={`mx-3 max-w-[6.5625rem] self-end py-[0.66rem] px-[2.56rem] border-white rounded-[0.575rem] text-[0.70rem] font-normal leading-[0.90rem]`}
+        className={`mx-3 max-w-[6.5625rem] self-end py-[0.66rem] px-[2.56rem] border-white rounded-[0.575rem] text-[0.70rem] font-normal leading-[0.90rem] ${
+          animate
+            ? " opacity-100transition-all duration-500 delay-[2000ms] ease-in-out"
+            : " opacity-0 "
+        }`}
         // navigateUrl={""}
         title={"Skip"}
       />
 
-      <div className="flex flex-col gap-y-11 ">
+      <div className="flex flex-col gap-y-11  ">
         <img
-          className="max-h-16 max-w-full h-auto object-contain self-center"
+          className={`max-h-16 max-w-full h-auto object-contain self-center ${
+            animate
+              ? "translate-y-0 opacity-100 scale-100 transition-all duration-500 delay-100 ease-in-out"
+              : "-translate-y-40 opacity-0 scale-50"
+          }`}
           src="/images/himalayan-logo.png"
           alt="logo"
         />
 
         <div className=" flex flex-col gap-y-[24.7px]">
-          <h1 className="text-textPink font-Poppins text-xl font-bold leading-[1.625rem] text-center">
+          <h1
+            className={`text-textPink font-Poppins text-xl font-bold leading-[1.625rem] text-center ${
+              animate
+                ? " opacity-100 scale-100 transition-all duration-500 delay-1000 ease-in-out"
+                : " opacity-0 scale-0"
+            }`}
+          >
             Celebrate Himalayan <br /> day with us!
           </h1>
 
-          <p className="font-Poppins font-normal text-[17px] leading-6  text-center text-black/80">
+          <p
+            className={`font-Poppins font-normal text-[17px] leading-6  text-center text-black/80 ${
+              animate
+                ? " opacity-100transition-all duration-500 delay-1000 ease-in-out"
+                : " opacity-0 "
+            }`}
+          >
             This Himalayan day let us take <br /> a stand to uplift the
             livelihood <br /> of the Kashmiri Saffron farmers
           </p>
@@ -38,7 +63,11 @@ const Welcome = () => {
       <div className="flex flex-col gap-y-3 ">
         <Button
           onClick={() => navigate("/batch-input")}
-          className={`max-w-[19.5rem] text-nowrap self-center py-[14px] rounded-[0.8625rem] border-white/50 font-semibold text-[1.046875rem] leading-[1.354375rem] px-[6.75rem]`}
+          className={`max-w-[19.5rem] text-nowrap self-center py-[14px] rounded-[0.8625rem] border-white/50 font-semibold text-[1.046875rem] leading-[1.354375rem] px-[6.75rem] ${
+            animate
+              ? " opacity-100transition-all duration-500 delay-[2000ms] ease-in-out"
+              : " opacity-0 "
+          } `}
           // navigateUrl={"/batch-input"}
           title={"Get Started"}
         />
