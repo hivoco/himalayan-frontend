ad95c58b56b04b46ac41d5a3c94d83cd9893098d
Krishna-hivoco <krishna@hivoco.com>
2024-08-22 18:40:23 +0530

    API integration done

--- src/instance.js
+++ src/instance.js
@@ -3,8 +3,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8805",
-  // baseURL: "https://api.himalayansaffron.in",
+  // baseURL: "http://localhost:8805",
+  baseURL: "https://api.himalayansaffron.in",
   headers: {
     "Content-Type": "application/json",
   },
